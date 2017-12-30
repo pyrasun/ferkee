261a69c1b42c0efba61641e4808dc191cbdf7f66
Mike Spille <michael.spille@gmail.com>
2017-12-30 12:18:32 -0500

    Start full Python-only implementation

---
+++ ferkeeRun.py
@@ -0,0 +1,15 @@
+#!python
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from scrapy.utils.log import configure_logging
+
+configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
+
+process = CrawlerProcess(get_project_settings())
+
+# 'followall' is the name of one of the spiders of the project.
+process.crawl('ferkee', domain='ferc.gov')
+process.start() # the script will block here until the crawling is finished
+
+
