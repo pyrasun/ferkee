a81ceba2f8d31fb291935e9f4fb978bd1f77801f
Mike Spille <michael.spille@gmail.com>
2018-01-05 19:57:46 -0500

    Add in python level properties

--- ferkeeRun.py
+++ ferkeeRun.py
@@ -1,27 +1,32 @@
 import sys
 import pprint
 import ConfigParser as configparser
+import argparse
 
-
+import ferkee_props
 
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scrapy.utils.log import configure_logging
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--properties", help="Path to ferkee properties file", action="store")
+parser.add_argument("--nodb", action="store_true")
+args = parser.parse_args()
+
 pp = pprint.PrettyPrinter(indent=4)
 
 config = configparser.RawConfigParser()
-config.read(sys.argv[1])
-properties = dict(config.items("Ferkee"))
+config.read(args.properties)
+ferkee_props.props = dict(config.items("Ferkee"))
 
-pp.pprint (properties)
+pp.pprint (ferkee_props.props)
 
 configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
 
 process = CrawlerProcess(get_project_settings())
 
-# 'followall' is the name of one of the spiders of the project.
-process.crawl('ferkee', domain='ferc.gov', argNoDBMode=True)
+process.crawl('ferkee', domain='ferc.gov', argNoDBMode=args.nodb)
 process.start() # the script will block here until the crawling is finished
 
 
